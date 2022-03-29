b16200a9ad797fb8317b0e939ea913d682fd29e2
Dana <alu0101328348@ull.edu.es>
2022-03-29 10:00:28 +0100

    :]

---
+++ practica5-MultBinariaSnowAEs/build/CMakeFiles/snow3G-AES.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snow3G-AES.dir/mnt/c/Users/uSer/OneDrive/Escritorio/SSI/practica5-MultBinariaSnowAEs/snow3G-AES.cpp.o"
+  "snow3G-AES.pdb"
+  "snow3G-AES"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/snow3G-AES.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
